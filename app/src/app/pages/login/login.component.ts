b6447623957c09fd98d52b099d7cfe3b0ebd4e6f
trak-dev <yann.picaud@gmail.com>
2024-05-15 17:48:15 +0200

    wip

--- app/src/app/pages/login/login.component.ts
+++ app/src/app/pages/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { CommonModule } from '@angular/common';
+import { CheckboxModule } from 'primeng/checkbox';
+import { MessageService } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    DialogModule,
+    CommonModule,
+    CheckboxModule,
+    FormsModule
+  ],
+  providers: [MessageService],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  displayLogin: boolean = true;
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  magicLogin: boolean = false;
+
+  async login() {
+    if (!this.email || !this.name) return 
+    if (this.magicLogin) {
+      // Handle magic login
+      console.log('Magic login with email:', this.email);
+    } else {
+      // Handle regular login
+      console.log('Regular login with email:', this.email, 'and password:', this.password);
+    }
+  }
 }
