f0fd4cc1b103af617b0d45bfb1298e8cfd6e97fd
trak-dev <yann.picaud@gmail.com>
2024-05-15 11:54:43 +0200

    continue upgrading front

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AuthGuard }  from  './guards/auth-guard.guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'register', component: RegisterComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
